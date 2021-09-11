a5f35dab11ab7e168740288a40894b151bbac2e3
Marcos <marcosbadillo089@gmail.com>
2021-09-11 12:40:41 -0500

    created a components folder in src and created a folder for About and Nav with index.js

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+        <p id="about">I attended the Full-Stack Web Development at The University of Texas at Austin coding bootcamp and received my certificate. 
+        I am an entry-level Software Developer seeking full time opportunities developing full stack technologies and APIs  .</p>
+      </section>
+    );
+}
+
+export default About;
