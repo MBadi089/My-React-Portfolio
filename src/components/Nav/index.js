8d1bf1518f1853b69fb9731dc4701988ec4ea022
Marcos <marcosbadillo089@gmail.com>
2021-09-12 11:46:13 -0500

    updated Nav/index.js code with a header and unordered list

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,34 @@ import { capitalizeFirstLetter } from "../../utils/helpers";
 function Nav(props) {
 
     return (
-        <header>            
+        <header>
+            <h2>
+                <a href="/"> Marcos Badillo </a>
+            </h2>
+            <nav>
+                <ul className ="flex-row">
+                    <li className="mx-3">
+                        <a href="#about">
+                            About Me
+                        </a>
+                    </li>
+                    <li className="mx-3">
+                        <a href="#projects">
+                            Projects
+                        </a>
+                    </li>
+                    <li className="mx-3">
+                        <a href="#github">
+                            My GitHub
+                        </a>
+                    </li>
+                    <li className="mx-3">
+                        <a href="#contact">
+                            Contact Me
+                        </a>
+                    </li>
+                </ul>
+            </nav>            
         </header>
     );
 }
