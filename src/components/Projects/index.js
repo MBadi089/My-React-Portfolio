3ee90effd87e080db5647033848a1648f30dc3db
Marcos <marcosbadillo089@gmail.com>
2021-09-17 13:02:04 -0500

    updated code in Project/index.js and index.css

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,7 +4,7 @@ import img2 from '../../images/UTA-Project2.png';
 
 export default function Projects() {
     return (
-        <div>
+        <div className="projectImages">
             <h1>My Projects</h1>
             <li><a href="https://github.com/MBadi089/Glad-You-Game"><img src={img1}/></a></li>
             <li><a href="https://github.com/AlfonsoCerv7/MovieReddy"><img src={img2}/></a></li>
