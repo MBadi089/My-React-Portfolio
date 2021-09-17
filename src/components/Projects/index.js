bbc742d6caabf19a148e3a4948ceb29b487d27ce
Marcos <marcosbadillo089@gmail.com>
2021-09-17 12:40:42 -0500

    updated code in Project/index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import img from '../../../public/ProjectImages';
+import img from '../../images/UTA-Project3.png';
 
 export default function Projects() {
     return (
         <div>
             <h1>My Projects</h1>
-            <li><a href="https://github.com/MBadi089/Glad-You-Game"><img src="UTA Project3.png"/></a></li>
+            <li><a href="https://github.com/MBadi089/Glad-You-Game"><img src={img}/></a></li>
         </div>
     )
 }
