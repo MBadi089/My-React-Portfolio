35d6219fa407b183a72ca906cd4d49acc8b27a9d
Marcos <marcosbadillo089@gmail.com>
2021-09-17 12:18:22 -0500

    updated img src in Projects/index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,7 +5,7 @@ export default function Projects() {
     return (
         <div>
             <h1>My Projects</h1>
-            <li><a href="https://github.com/MBadi089/Glad-You-Game"><img src={img} alt={"UTA Project3"}/></a></li>
+            <li><a href="https://github.com/MBadi089/Glad-You-Game"><img src="UTA Project3.png"/></a></li>
         </div>
     )
 }
