cf2404b86b25da05eb2fcc81081fc09b08e1a874
Marcos <marcosbadillo089@gmail.com>
2021-09-18 13:57:48 -0500

    updated code in index.css to style Nav/index.js

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,29 +4,35 @@ import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
     return (
+<div className="nav-container">        
+   <section className="headerName">                        
+        <Link to="/">
+            <li className="myName-link">Marcos Badillo</li>
+        </Link>
+   </section>
+
         <nav>
-            <ul className="container-links">
-                <Link to="/">
-                    <li className="myName-link">Marcos Badillo</li>
-                </Link>
+            <ul>
+                <div className="container-links">
+                    <Link to="/about">
+                        <li className="nav-link">About Me</li>
+                    </Link>
 
-                <Link to="/about">
-                    <li className="nav-link">About Me</li>
-                </Link>
+                    <Link to="/projects">
+                        <li className="nav-link">Projects</li>
+                    </Link>
 
-                <Link to="/projects">
-                    <li className="nav-link">Projects</li>
-                </Link>
+                    <Link to="/certificate">
+                        <li className="nav-link">Certificate</li>
+                    </Link>
 
-                <Link to="/certificate">
-                    <li className="nav-link">Certificate</li>
-                </Link>
-
-                <Link to="/contact">
-                    <li className="nav-link">Contact Me</li>
-                </Link>
+                    <Link to="/contact">
+                        <li className="nav-link">Contact Me</li>
+                    </Link>
+                 </div>    
             </ul>
         </nav>
+</div>        
     );
 }
 
