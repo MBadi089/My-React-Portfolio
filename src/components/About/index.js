93bfc74a5e6d71fae3f5e8a7255b416cdf1767f4
Marcos <marcosbadillo089@gmail.com>
2021-09-17 22:24:38 -0500

    updated code in About/index.js and the Me.png is appearing now

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import img from '../../images/Me';
+import image from '../../images/Me.png';
 
 function About() {
     return (
-        <div class="container">
-          <h1>Marcos Badillo</h1>
-          <h2>Entry Level Software Developer</h2>
-          <div class="image" img src={img}></div>
-          <p id="about">I attended the Full-Stack Web Development at The University of Texas at Austin coding bootcamp and received my certificate. 
-                        I am an entry-level Software Developer seeking full time opportunities developing full stack technologies and APIs  .
-          </p>
+      <div className="about-container">
+        <h1>Marcos Badillo</h1>
+        <h2>Entry Level Software Developer</h2>
+        <img src={image}/>
+        <p className="about">I attended the Full-Stack Web Development at The University of Texas at Austin coding bootcamp and received my certificate. 
+                            I am an entry-level Software Developer seeking full time opportunities developing full stack technologies and APIs  .
+        </p>
       </div>
     );
 }
