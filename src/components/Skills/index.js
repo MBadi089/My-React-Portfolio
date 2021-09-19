46c8cd24b4001324fd428915a6ac0297045ba1f9
Marcos <marcosbadillo089@gmail.com>
2021-09-19 15:05:42 -0500

    added code to Skills/index.js and styling in index.css

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import imgLogo1 from '../../images/reactLogo.png';
+import imgLogo2 from '../../images/javascriptLogo.png';
+import imgLogo3 from '../../images/htmlLogo.png';
+import imgLogo4 from '../../images/cssLogo.png';
 
 export default function Skills() {
     return (
-        <div>
+        <div className ="skills-container">
             <h1>My Skills</h1>
+
+            <section className="imageLeft">
+                <img src={imgLogo1}/>
+                <p className="textRight">asdfasdfasdfasdfasdfasdf</p>
+            </section>
+
+            <section className="imageRight">
+                <img src={imgLogo2}/>
+                <p className="textLeft">asdfasdfasdfasdfasdf</p>
+            </section>
+
+            <section className="imageLeft">
+                <img src={imgLogo3}/>
+                <p className="textRight">asfasdfasdfasdfasd</p>
+            </section>
+
+            <section className="imageRight">
+                <img src={imgLogo4}/>
+                <p className="textLeft">asdfasdfasdfasdfa</p>
+            </section>
         </div>
     )
 }
