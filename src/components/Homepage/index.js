8e3495366e9ffeeeb102d8b8dbc84ae0870e9f57
Marcos <marcosbadillo089@gmail.com>
2021-09-19 16:14:26 -0500

    updated Homepage/index.js and index.css

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,11 +3,11 @@ import image from '../../images/Me.png';
 
 function Homepage() {
     return (
-      <div className="about-container">
+      <div className="homepage-container">
         <h1>Marcos Badillo</h1>
         <h2>Entry Level Software Developer</h2>
         <img src={image} />
-        <p className="about">I attended the Full-Stack Web Development at The University of Texas at Austin coding bootcamp and received my certificate. 
+        <p className="about-me">I attended the Full-Stack Web Development at The University of Texas at Austin coding bootcamp and received my certificate. 
                             I am an entry-level Software Developer seeking full time opportunities developing full stack technologies and APIs  .
         </p>
       </div>
