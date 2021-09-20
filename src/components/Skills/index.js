226cc7fdb35c14e7c4584e413e63471a275f4799
Marcos <marcosbadillo089@gmail.com>
2021-09-20 18:21:59 -0500

    updated styling in index.css

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -20,7 +20,7 @@ export default function Skills() {
             <section className="row">                
                 <div className="textLeft col">
                     <h1 className="headerName">Javascript</h1>
-                    <p>JavaScript is a scripting or programming language that allows you to implement complex features on web pages</p>
+                    <p>JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.</p>
                 </div>
                     <img src={imgLogo2} className="col"/>
                 
