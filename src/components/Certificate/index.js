8f85e8d1d8b75e4a29118f408117e95d430ab970
Marcos <marcosbadillo089@gmail.com>
2022-03-14 00:03:00 -0500

    added video code for background in Certificate component index.js, background works

--- src/components/Certificate/index.js
+++ src/components/Certificate/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import image from '../../images/UTA-Certificate.png';
+import video from '../../components/video/RetroSciFi.mp4';
 
 export default function Certificate() {
     return (
         <div className ="certificate-container">
+            <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1"
+        }}
+        >
+          <source src={video} type="video/mp4" />
+        </video>
             <h1 className="mycertificate">My Certificate</h1>
             <li className="certImage"><a href="https://techbootcamps.utexas.edu/coding/"><img src={image} /></a></li>
         </div>
