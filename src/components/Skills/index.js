ce75b21ef2987d69865d49ccd9a1c7426a886857
Marcos <marcosbadillo089@gmail.com>
2022-03-14 00:11:19 -0500

    added video code to Skills component index.js, background covers half the page still works, edit later

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -3,10 +3,28 @@ import imgLogo1 from '../../images/reactLogo.png';
 import imgLogo2 from '../../images/javascriptLogo.png';
 import imgLogo3 from '../../images/htmlLogo.png';
 import imgLogo4 from '../../images/cssLogo.png';
+import video from '../../components/video/RetroSciFi.mp4';
 
 export default function Skills() {
     return (
         <div className ="skills-container">
+            <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1"
+        }}
+        >
+          <source src={video} type="video/mp4" />
+        </video>
             <h1 className="myskills">My Skills</h1>
 
             <section className="row">
