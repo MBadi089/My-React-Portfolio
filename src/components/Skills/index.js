84c73c4a2dd526575c9d728ef7be1b7ca6f67962
Marcos <marcosbadillo089@gmail.com>
2022-03-17 23:20:17 -0500

    cleaned code to look better in my skills index.js

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -29,10 +29,10 @@ export default function Skills() {
             <h1 class="myskills">My Skills</h1>
                 <div className="skills-container">
                     <Row>
-                    <Col className="imgLogo"><a href="https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started"><img src={imgLogo1} /></a></Col>
-                    <Col className="imgLogo"><a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript"><img src={imgLogo2} /></a></Col>
-                    <Col className="imgLogo"><a href="https://developer.mozilla.org/en-US/docs/Web/HTML"><img src={imgLogo3} /></a></Col>
-                    <Col className="imgLogo"><a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><img src={imgLogo4} /></a></Col>
+                        <Col className="imgLogo"><a href="https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started"><img src={imgLogo1} /></a></Col>
+                        <Col className="imgLogo"><a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript"><img src={imgLogo2} /></a></Col>
+                        <Col className="imgLogo"><a href="https://developer.mozilla.org/en-US/docs/Web/HTML"><img src={imgLogo3} /></a></Col>
+                        <Col className="imgLogo"><a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><img src={imgLogo4} /></a></Col>
                     </Row>
                 </div>
             </div>
