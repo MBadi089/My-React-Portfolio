df74ce7a6f7a3d533d4642530d4bb0b6e16b59c0
Marcos <marcosbadillo089@gmail.com>
2022-03-19 12:47:14 -0500

    commented out a validation code for the contact me index.js section, still can't make user input error messages work

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -18,6 +18,42 @@ export default function ContactMe() {
       e.target.reset()
     }
 
+    // document.getElementById("contact-me-form").onsubmit = function () {
+    //     var x = document.forms["contact-me-form"]["name"].value;
+    //     var y = document.forms["contact-me-form"]["email"].value;
+    //     var z = document.forms["contact-me-form"]["message"].value;
+    
+    //     var submit = true;
+    
+    //     if (x == null || x == "") {
+    //         nameError = "Please enter your name";
+    //         document.getElementById("name_error").innerHTML = nameError;
+    //         submit = false;
+    //     }
+    
+    //     if (y == null || y == "") {
+    //         emailError = "Please enter your email";
+    //         document.getElementById("email_error").innerHTML = emailError;
+    //         submit = false;
+    //     }
+    
+    //     if (z == null || z == "") {
+    //         messageError = "Please leave a message";
+    //         document.getElementById("message_error").innerHTML = messageError;
+    //         submit = false;
+    //     }
+    
+    //     return submit;
+    // }
+    
+    // function removeWarning() {
+    //     document.getElementById(this.id + "_error").innerHTML = "";
+    // }
+    
+    // document.getElementById("name").onkeyup = removeWarning;
+    // document.getElementById("email").onkeyup = removeWarning;
+    // document.getElementById("message").onkeyup = removeWarning;
+
     return (
         <div className="contact-container">
         <video
@@ -38,16 +74,19 @@ export default function ContactMe() {
           <source src={video} type="video/mp4" />
         </video>
             <h1 className="contactMe">Contact Me</h1>
-            <form onSubmit={sendEmail}>
+            <form  name="contact-me-form" id="contact-me-form" onSubmit={sendEmail}>
                 <label for="userName">Your Name</label>
-                <input type="text" className="form-styling" placeholder="Your Name Here" name="name"/>
+                <input type="text" className="form-styling" placeholder="Your Name Here" id="name" name="name"/>
+                {/* <span class="error"><p id="name_error"></p></span> */}
 
                 <label for="userEmail">Your Email</label>
-                <input type="text" className="form-styling" placeholder="Email Address" name="email"/>
+                <input type="text" className="form-styling" placeholder="Email Address" id="email" name="email"/>
+                {/* <span class="error"><p id="email_error"></p></span> */}
 
                 <label for="userSubject">Subject</label>
                 <input type="text" className="form-styling" placeholder="Subject" name="subject"/>
-                <textarea className="form-styling messageBox" placeholder="Your Message Here" name="message"></textarea>
+                <textarea className="form-styling messageBox" placeholder="Your Message Here" id="message" name="message"/>
+                {/* <span class="error"><p id="message_error"></p></span> */}
 
                 <input type="submit" className="btn" value="Send Message"></input>
             </form>
