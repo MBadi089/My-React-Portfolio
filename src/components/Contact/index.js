37af29b487ab5d7e67ed555a09eab3187740d1e1
Marcos <marcosbadillo089@gmail.com>
2022-03-25 20:44:07 -0500

    adding value{} to each of the form labels, getting 1 error for rendor() function, {} error is asking for a ;

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import video from '../../components/video/RetroSciFi.mp4';
 
@@ -7,8 +8,20 @@ export default function ContactMe() {
         email: '',
         message: '',
         emailStatus: ''
-
     }
+
+    handleChange = input => e => {
+      this.setState({
+          [input]: e.target.value
+      });
+    }
+
+    render(){
+      const {
+          name,
+          email,
+          message
+      } = this.state;
 
     return (
         <div className="contact-container">
@@ -33,16 +46,17 @@ export default function ContactMe() {
           <h2 className="contactMe">Contact Me</h2>
             <form id="contact-me-form">
                 <label for="userName">Your Name</label>
-                <input type="text" className="form-styling" placeholder="Your Name Here" onChange={this.handleChange('name')}/>
+                <input type="text" className="form-styling" placeholder="Your Name Here" value={name} onChange={this.handleChange('name')}/>
 
                 <label for="userEmail">Your Email</label>
-                <input type="email" className="form-styling" placeholder="Email Address" onChange={this.handleChange('email')}/>
+                <input type="email" className="form-styling" placeholder="Email Address" value={email} onChange={this.handleChange('email')}/>
 
                 <label for="messageMe">Message Me</label>
-                <textarea className="form-styling messageBox" placeholder="Leave A Message Here" onChange={this.handleChange('message')}/>
+                <textarea className="form-styling messageBox" placeholder="Leave A Message Here" value={message} onChange={this.handleChange('message')}/>
 
                 <input type="submit" className="btn" value="Send Message"></input>
             </form>
         </div>
-    )
+    );
+  }
 }
