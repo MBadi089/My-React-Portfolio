b5c6399e26affd7b866829475b018c2c6c88fd39
Marcos <marcosbadillo089@gmail.com>
2022-03-26 15:03:07 -0500

    added emailjs code for contact index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,21 @@
-// import { render } from '@testing-library/react';
 import React from 'react';
+import emailjs from 'emailjs-com';
 import video from '../../components/video/RetroSciFi.mp4';
 
 export default function ContactMe() {
 
+    function sendEmail(e){
+      e.preventDefault();
+
+      emailjs.sendForm('gmail', 'gmail_template', e.target, 'user_XsYqdvu1bqpaekVm805ua')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        e.target.reset()
+    };
 
     return (
         <div className="contact-container">
@@ -26,15 +38,18 @@ export default function ContactMe() {
           </video>
         
           <h2 className="contactMe">Contact Me</h2>
-            <form id="contact-me-form">
+            <form onSubmit={sendEmail} id="contact-me-form">
                 <label for="userName">Your Name</label>
-                <input type="text" className="form-styling" placeholder="Your Name Here" id="name"/>
+                <input type="text" className="form-styling" placeholder="Your Name Here" name="name"/>
 
                 <label for="userEmail">Your Email</label>
-                <input type="email" className="form-styling" placeholder="Email Address" id="email"/>
+                <input type="email" className="form-styling" placeholder="Email Address" name="email"/>
+
+                <label for="subject">Subject</label>
+                <input type="subject" className="form-styling" placeholder="Subject" name="subject"/>
 
                 <label for="messageMe">Message Me</label>
-                <textarea className="form-styling messageBox" placeholder="Leave A Message Here" id="message"/>
+                <textarea className="form-styling messageBox" placeholder="Leave A Message Here" name="message"/>
 
                 <input type="submit" className="btn" value="Send Message"></input>
             </form>
