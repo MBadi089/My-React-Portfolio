54bb3f52ba74bb1afb3ab66b3be124db6c39439c
Marcos <marcosbadillo089@gmail.com>
2022-03-27 21:35:30 -0500

    updated contact form with validation, need to work add email code for validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,73 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import emailjs from 'emailjs-com';
 import video from '../../components/video/RetroSciFi.mp4';
 
 export default function ContactMe() {
+const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userSubject, setUserSubject] = useState("");
+    const [userMessage, setUserMessage] = useState("");
+    
+    const [userNameErr, setUserNameErr] = useState({});
+    const [userEmailErr, setUserEmailErr] = useState({});
+    const [userSubjectErr, setUserSubjectErr] = useState({});
+    const [userMessageErr, setUserMessageErr] = useState({});
 
     function sendEmail(e){
+
       e.preventDefault();
+      const isValid = formValidation();
 
       emailjs.sendForm('service_gi1yvcz', 'gmail_template', e.target, 'user_XsYqdvu1bqpaekVm805ua')
         .then((result) => {
             console.log(result.text);
-            alert('Your Message was sent!!')
         }, (error) => {
             console.log(error.text);
         });
 
         e.target.reset()
+    }
+
+    const formValidation = () => {
+        const userNameErr = {};
+        const userEmailErr = {};
+        const userSubjectErr = {};
+        const userMessageErr = {};
+        let isValid = true;
+
+        if(userName.trim().length < 5) {
+            userNameErr.userNameShort = "Please enter your full name";
+            isValid = false;
+        }
+
+        //an if statement for email address
+
+        if(userSubject.trim().length < 3) {
+            userSubjectErr.userSubjectShort = "Please enter more than 3 characters in the subject box";
+            isValid = false;
+        }
+
+        if(userSubject.trim().length > 20) {
+            userSubjectErr.userSubjectLong = "Please keep the characters under 20 in the subject box";
+            isValid = false;
+        }
+
+        if(userMessage.trim().length < 4) {
+            userMessageErr.userMessageShort = "Please enter more than 4 characters in the message box";
+            isValid = false;
+        }
+
+        if(userMessage.trim().length > 300) {
+            userMessageErr.userMessageLong = "Please enter less than 300 characters in the message box";
+            isValid = false;
+        }
+
+        setUserNameErr(userNameErr);
+        setUserEmailErr(userEmail);
+        setUserSubjectErr(userSubject);
+        setUserMessageErr(userMessage);
+        return isValid;
+        
     }
 
     return (
@@ -41,16 +93,24 @@ export default function ContactMe() {
           <h2 className="contactMe">Contact Me</h2>
             <form onSubmit={sendEmail} id="contact-me-form">
                 <label for="userName">Your Name</label>
-                <input type="text" className="form-styling" placeholder="Your Name Here" name="name"/>
+                <input type="text" className="form-styling" placeholder="Your Name Here" value={userName} onChange={(e)=>{setUserName(e.target.value)}}/>
+                {Object.keys(userNameErr).map((key)=>{
+                    return <div style={{color : "red"}}>{userNameErr[key]}</div>})}
 
                 <label for="userEmail">Your Email</label>
-                <input type="email" className="form-styling" placeholder="Email Address" name="email"/>
+                <input type="email" className="form-styling" placeholder="Email Address" value={userEmail} onChange={(e)=>{setUserEmail(e.target.value)}}/>
+                {Object.keys(userEmailErr).map((key)=>{
+                    return <div style={{color : "red"}}>{userEmailErr[key]}</div>})}
 
                 <label for="subject">Subject</label>
-                <input type="subject" className="form-styling" placeholder="Subject" name="subject"/>
+                <input type="subject" className="form-styling" placeholder="Subject" value={userSubject} onChange={(e)=>{setUserSubject(e.target.value)}}/>
+                {Object.keys(userSubjectErr).map((key)=>{
+                    return <div style={{color : "red"}}>{userSubjectErr[key]}</div>})}
 
                 <label for="messageMe">Message Me</label>
-                <textarea className="form-styling messageBox" placeholder="Leave A Message Here" name="message"/>
+                <textarea className="form-styling messageBox" placeholder="Leave A Message Here" value={userMessage} onChange={(e)=>{setUserMessage(e.target.value)}}/>
+                {Object.keys(userMessageErr).map((key)=>{
+                    return <div style={{color : "red"}}>{userMessageErr[key]}</div>})}
 
                 <input type="submit" className="btn" value="Send Message"></input>
             </form>
